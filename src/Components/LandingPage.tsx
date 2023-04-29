b349bbfcd303ad20c54624b906d368ccde9d13d2
Yasmeen <yelzamek@udel.edu>
2023-04-29 17:34:33 -0400

    Changes show up on page now! Also landing page component created.

---
+++ src/Components/LandingPage.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { Col, Form, Row } from "react-bootstrap";
+
+export function LandingPage({
+    defaultZip
+}: {
+    defaultZip: string;
+}): JSX.Element {
+    type ChangeEvent = React.ChangeEvent<
+        HTMLTextAreaElement | HTMLInputElement | HTMLSelectElement
+    >;
+
+    const [zip, setZip] = useState<string>("");
+
+    function updateZip(event: ChangeEvent) {
+        setZip(event.target.value);
+    }
+
+    return (
+        <div>
+            <h3>Zip</h3>
+            <Form.Group controlId="formSearchZip" as={Row}>
+                <Form.Label column sm={0}>
+                    Enter Zip Code
+                </Form.Label>
+                <Col>
+                    <Form.Control
+                        type="text"
+                        value={zip}
+                        onChange={updateZip}
+                    ></Form.Control>
+                </Col>
+            </Form.Group>
+            <div>Your answer: {defaultZip}</div>
+        </div>
+    );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,18 @@
+// You will not need to modify this file
+
 import React from "react";
-import { render } from "react-dom";
+import ReactDOM from "react-dom";
 import "./index.css";
+import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-function App() {
-    return (
-        <div className="App">
-            <p>UD HenHacks</p>
-            <br></br>
-        </div>
-    );
-}
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById("root")
+);
 
-const rootElement = document.getElementById("root");
-render(<App />, rootElement);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
