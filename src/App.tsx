9bea3013756b842c516986083219f73f9a643110
Yasmeen <yelzamek@udel.edu>
2023-04-29 21:27:23 -0400

    twitter embedded on page! needs resizing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import "./App.css";
 import { LandingPage } from "./Components/LandingPage";
+import {
+    TwitterTimelineEmbed,
+    TwitterHashtagButton
+} from "react-twitter-embed";
 
 function App(): JSX.Element {
     return (
@@ -10,6 +14,16 @@ function App(): JSX.Element {
                 <div className="ZipBar">
                     <LandingPage defaultZip="19717"></LandingPage>
                 </div>
+            </header>
+            <header className="Twitter-embedding">
+                <TwitterTimelineEmbed
+                    sourceType="profile"
+                    screenName="DelawareEMA"
+                    options={{ height: 400 }}
+                />
+                <TwitterHashtagButton
+                    tag={"MyAnklesAreInWaterRn"}
+                ></TwitterHashtagButton>
             </header>
         </div>
     );
